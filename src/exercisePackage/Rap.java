df4088c73dc7ff08c6ad17807182a4453db70573
femarleycode <femarleycode@gmail.com>
2020-03-25 11:01:04 +0000

    2nd commit

---
+++ src/exercisePackage/Rap.java
@@ -0,0 +1,14 @@
+
+//beginner 4 (not working)
+//public class Rap {
+//	public String rapLyrics(String Lyrics) {	
+//			Return lyrics();
+//	}
+//}
+
+//beginner3
+//public class Rap {
+//	public void rapLyrics(String lyrics) {
+//		System.out.println(lyrics);
+//	}
+//}
---
+++ src/exercisePackage/Runner.java
@@ -0,0 +1,35 @@
+package exercisePackage;
+
+//beginner 4 (not working)
+//public class Runner {
+//	public static void main(String[] args) {
+//
+//		String lyrics = "yo yo yo it's me";
+//		Rap irap = new Rap();
+//		irap.rapLyrics(lyrics);
+//
+//		System.out.println(irap.rapLyrics(lyrics));
+//
+//	}
+//}
+
+//beginner3
+//public class Runner {
+//	public static void main(String[] args) {
+//
+//		String lyrics = "yo yo yo it's me";
+//		Rap irap = new Rap();
+//		irap.rapLyrics(lyrics);
+//
+//	}
+//}
+
+/**
+ * //beginner 1&2 public class Runner {
+ * 
+ * public static void main (String[]args) {
+ * 
+ * String Greeting = "Hello World"; System.out.println(Greeting);
+ * 
+ * } }
+ */
